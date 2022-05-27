85d06ff047ebcdcfee2f55eb36bb0aedb8b37bf6
pikachu2022 <harrison.wu2003@gmail.com>
2022-05-27 18:17:26 +0800

    Create 123.js

---
+++ 123.js
@@ -0,0 +1,132 @@
+/*var a;
+function poiu(){
+	a=Number(document.getElementById("num").value);
+	c=a+1;
+}
+//a=parseInt(a);
+var b=9;
+var c;
+if(a>0){
+	a=-1;
+}
+function showAlert( lockTime )
+{
+    alert( 5 );
+    if( confirm( '你確定要傳送此數值嗎？' ) )
+    {
+        document.getElementById( 'dataID' ).value = 'Yes';
+    }
+}
+*/
+function analyze(){
+var d=new Date();
+var e=d.getDay();//星期
+//document.write("pikachu2022.github.io");
+//window.location.href="pikachu2022.github.io";
+//window.open("pikachu2022.github.io");
+//var mystr=new String("我的網站");
+//document.write('<font size="27";>mystr.link("https://pikachu2022.github.io")</font>');
+var f=d.getHours();//小時
+if(e==1){
+	if(f==8)
+		window.open("https://meet.google.com/hyd-oyxf-oof");
+	else if(f==9)
+		window.open("https://meet.google.com/hyd-oyxf-oof");
+	else if(f==10)
+		window.open("https://meet.google.com/nrw-jvrv-puc");
+	else if(f==11)
+		window.open("https://meet.google.com/tmc-zhnz-mev");
+	else if(f==13)
+		window.open("https://meet.google.com/cbt-rrfj-yrc");
+	else if(f==14)
+		window.open("https://meet.google.com/tma-houx-nbd");
+	else if(f==15)
+		window.open("https://meet.google.com/tma-houx-nbd");
+}
+else if(e==2){
+	if(f==8)
+		window.open("https://meet.google.com/tmc-zhnz-mev");
+	else if(f==9)
+		window.open("https://meet.google.com/tum-yuda-zsq");
+	else if(f==10)
+		window.open("https://meet.google.com/nrw-jvrv-puc");
+	else if(f==11)
+		window.open("https://meet.google.com/nrw-jvrv-puc");
+	else if(f==13)
+		window.open("https://meet.google.com/wbs-fjhk-sue");
+	else if(f==14)
+		window.open("https://meet.google.com/tma-houx-nbd");
+	else if(f==15)
+		window.open("https://meet.google.com/tma-houx-nbd");
+}
+else if(e==3){
+	if(f==8)
+		window.open("https://meet.google.com/tma-houx-nbd");
+	else if(f==9)
+		window.open("https://meet.google.com/nrw-jvrv-puc");
+	else if(f==10)
+		window.open("https://meet.google.com/tmc-zhnz-mev");
+	else if(f==11)
+		window.open("https://meet.google.com/wbs-fjhk-sue");
+	else if(f==13)
+		window.open("https://meet.google.com/cbt-rrfj-yrc");
+	else if(f==14)
+		window.open("https://meet.google.com/tum-yuda-zsq");
+	else if(f==15)
+		window.open("https://meet.google.com/urg-ksux-wwq");
+}
+else if(e==4){
+	if(f==8)
+		window.open("https://meet.google.com/tum-yuda-zsq");
+	else if(f==9)
+		window.open("https://meet.google.com/nrw-jvrv-puc");
+	else if(f==10)
+		window.open("https://meet.google.com/gjt-aknv-pzn");
+	else if(f==11)
+		window.open("https://meet.google.com/wbs-fjhk-sue");
+	else if(f==13)
+		window.open("https://meet.google.com/tma-houx-nbd");
+	else if(f==14)
+		window.open("https://meet.google.com/qhk-xvaw-edi");
+	else if(f==15)
+		window.open("https://meet.google.com/qhk-xvaw-edi");
+}
+else if(e==5){
+	if(f==8)
+		window.open("https://");
+	else if(f==9)
+		window.open("https://");
+	else if(f==10)
+		window.open("https://meet.google.com/zti-xeis-hdz");
+	else if(f==11)
+		window.open("https://meet.google.com/zti-xeis-hdz");
+	else if(f==13)
+		window.open("https://meet.google.com/wbs-fjhk-sue");
+	else if(f==14)
+		window.open("https://meet.google.com/tma-houx-nbd");
+	else if(f==15)
+		window.open("https://meet.google.com/tma-houx-nbd");
+}
+
+
+
+
+
+}	
+
+
+
+
+if(new Date().getDay()==0 && new Date().getDay()==6)
+	document.getElementById('apple').textContent = '放假~';
+else if(new Date().getHours()>15)
+	document.getElementById('apple').textContent = '放學~';
+else if(new Date().getHours()<8)
+	document.getElementById('apple').textContent = '早自習';
+else if(new Date().getHours()<13 && new Date().getHours()>11)
+	document.getElementById('apple').textContent = '午休';
+/*
+console.log(a);
+console.log(b);
+console.log(c);
+*/
